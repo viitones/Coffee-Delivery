0280958b0dfadb15121545fbcd7c83d31378886d
viitones <vitonesmartins@gmail.com>
2024-10-20 12:03:02 -0300

    base configs

---
+++ src/components/index.tsx
@@ -0,0 +1,5 @@
+export function ComponenteOne() {
+  return (
+    <h1>componente 1</h1>
+  )
+}
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,20 @@
+export const defaultTheme = {
+  white: '#FFFFFF',
+  'gray-100': '#FAFAFA',
+  'gray-300': '#F3F2F2',
+  'gray-400': '#EDEDED',
+  'gray-500': '#E6E5E5',
+  'gray-600': '#D7D5D5',
+  'gray-700': '#8D8686',
+  'gray-800': '#574F4D',
+  'gray-900': '#403937',
+  black: '#272221',
+
+  'purple-light': '#EBE5F9',
+  purple: '#8047F8',
+  'purple-dark': '#4B2995',
+
+  'yellow-light': '#F1E9C9',
+  yellow: '#DBAC2C',
+  'yellow-dark': '#C47F17',
+} as const
